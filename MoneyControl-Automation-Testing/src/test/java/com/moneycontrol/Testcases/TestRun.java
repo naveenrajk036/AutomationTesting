fa928666091a7c0ad95b2f6ff42af1a7f586d596
naveenrajk036 <naveenrajkumar36@gmail.com>
2023-09-19 20:14:32 +0530

    Java with Selenium Automation

---
+++ MoneyControl-Automation-Testing/src/test/java/com/moneycontrol/Testcases/TestRun.java
@@ -0,0 +1,53 @@
+package com.moneycontrol.Testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.moneycontrol.Base.Base;
+import com.moneycontrol.Pages.*;
+
+
+/*Provide the login credentials in 
+ * \src\test\resources\ObjectRepository\configProperties.properties 
+ * before executing the test
+ */
+public class TestRun extends Base {
+	WebDriver driver;
+	HomePage homepage = new HomePage();
+	GlobalMarkets gm = new GlobalMarkets();
+	Portfolio portfolio = new Portfolio();
+	
+	@BeforeTest
+	public void invokeBrowser() throws IOException, InterruptedException {
+		homepage.invokeBrowser();	
+	}
+	
+	@Test
+	public void test1() throws IOException, InterruptedException {
+		homepage.marketsPage();
+		homepage.login();
+	}
+	
+	@Test(dependsOnMethods = { "test1" })
+	public void test3() throws InterruptedException, IOException {
+		portfolio.addStock();
+		portfolio.sellStock();
+	}
+	
+	
+	@Test(enabled = false)
+	public void test2() throws IOException, InterruptedException {
+		homepage.marketsPage();
+		gm.globalMarkets();
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		tearDown();
+	}
+}
